d59f617ff131abef107804f7314cda9ff706969f
David Moreno Montero <dmoreno@tuxum.com>
2010-06-09 23:41:22 +0200

    Changed copyright from 2007-2008 to 2007-2010. Wow I started long time ago to almost forget it.

--- src/response.cpp
+++ src/response.cpp
@@ -1,6 +1,6 @@
 /**
     onion+ - web small modular server.
-    Copyright (C) 2007-2008 David Moreno Montero
+    Copyright (C) 2007-2010 David Moreno Montero
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
